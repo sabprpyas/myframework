f13d21a3c568477526b6d42114ceee3a34339f7b
sabprpyas <zzc05797084421@163.com>
2021-03-31 15:01:42 +0800

    update dispatcher

--- src/java/com/qinliming/frame/ann/After.java
+++ src/java/com/qinliming/frame/ann/After.java
@@ -5,10 +5,19 @@
  */
 package com.qinliming.frame.ann;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  *
  * @author qinliming
  */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface After {
     
 }
